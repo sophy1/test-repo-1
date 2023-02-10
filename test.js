442d7c33c088061e32a73d1b4e121ab971eb540b
sophy1 <jihye.sophy.won@gmail.com>
2023-02-10 15:01:52 +0900

    Update test.js

--- test.js
+++ test.js
@@ -13,3 +13,18 @@ var myArray = ['a', 'b', 'c', 'd'];
 
 delete myArray[2];  // Noncompliant. myArray => ['a', 'b', undefined, 'd']
 console.log(myArray[2]); // expected value was 'd' but output is undefined
+
+function multiply(a = 1, b) {  // Noncompliant
+  return a*b;
+}
+
+var x = multiply(42);  // returns NaN as b is undefined
+
+
+function MyClass(name, strings) {
+  name = foo;                    // Noncompliant
+
+  for (var str of strings) {
+    str = "";  // Noncompliant
+  }
+}
